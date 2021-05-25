b0fb42b2fd663a41a726ebae118d3733c2277d88
Sam Jaques <sam.jaques@barco.com>
2021-05-25 17:40:21 +0200

    Install git/vim/sshpass/screen/cmake/python3/pip3a/jq

--- install_common_tools.sh
+++ install_common_tools.sh
@@ -4,4 +4,11 @@ source install_helpers.sh
 
 install_tool rsync
 install_tool vim
+install_tool git
+install_tool screen
+install_tool jq
+install_tool cmake
+install_tool python3
+install_tool python3-pip
+install_tool ssh-pass
 
