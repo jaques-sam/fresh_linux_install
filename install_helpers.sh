4723a9ee3d1587d171b8f8b81d4513ccb9529b72
Jaques(EXT), Sam <sam.jaques@barco.com>
2023-01-02 10:47:37 +0100

    fix: spellcheck warnings

--- install_helpers.sh
+++ install_helpers.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 
 function os_env()
@@ -11,12 +10,13 @@ function os_env()
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${unameOut}"
   esac
-  echo ${machine}
+  echo "${machine}"
 }
 
 function os_installer()
 {
-  local machine="$(os_env)"
+  local machine
+  machine="$(os_env)"
   case "${machine}" in
     Linux*)
         installer="sudo apt-get";;
@@ -27,50 +27,52 @@ function os_installer()
     *)
         installer="echo \'no \'"
   esac
-  echo ${installer}
+  echo "${installer}"
 }
 
 function print_commence()
 {
+  local line
   local script_name="$1"
 
-  local line=$(printf "\n      %-30s%-s\n"  "${script_name:0:30}" " ")  # last string is used to fill
+  line=$(printf "\n      %-30s%-s\n"  "${script_name:0:30}" " ")  # last string is used to fill
   echo -e "${line// /_}"
 }
 
 function is_installed() {
   local tool_name="$1"
 
-  command -v ${tool_name} > /dev/null && echo -e "${tool_name} is already installed"
+  command -v "${tool_name}" > /dev/null && echo -e "${tool_name} is already installed"
 }
 
 function install_tool()
 {
   local tool_name="$1"
 
-  is_installed ${tool_name} && return
+  is_installed "${tool_name}" && return
   echo -e "\nInstalling ${tool_name}"
 
-  $(os_installer) install ${tool_name} -y
+  $(os_installer) install "${tool_name}" -y
 }
 
 function update_file()
 {
   local src_basename="$1"  # only name of the file
   local dest_filename="$2"  # filename (relative or absolute path)
+  local difference
 
   if [[ ! -f ${dest_filename} ]]; then
-    cp ${src_basename} ${dest_filename}
+    cp "${src_basename}" "${dest_filename}"
     return
   fi
 
-  local difference=$(diff ${dest_filename} ${src_basename} -u)
+  difference=$(diff "${dest_filename}" "${src_basename}" -u)
   if [[ -n "${difference}" ]]; then
     echo -e "${src_basename} file already exists, difference is:"
     echo -e "${difference}"
     echo -e "Find old file as /tmp/${src_basename}_old"
-    mv ${dest_filename} /tmp/${src_basename}_old
-    cp ${src_basename} ${dest_filename}
+    mv "${dest_filename}" "/tmp/${src_basename}_old"
+    cp "${src_basename}" "${dest_filename}"
   fi
 }
 
@@ -78,6 +80,6 @@ function git_repo_update()
 {
   local git_repo_dir="$1"
 
-  git -C ${git_repo_dir} checkout master -f
-  git -C ${git_repo_dir} pull -f
+  git -C "${git_repo_dir}" checkout master -f
+  git -C "${git_repo_dir}" pull -f
 }
